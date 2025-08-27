9b00d595645f643a3aba2192f1d1a1bdecef197a
Avinash <88026629+Avinashdev2011@users.noreply.github.com>
2025-08-27 22:01:53 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './**/*.html', // Include all HTML files in your project for Tailwind CSS classes
+    './src/**/*.js', // Optional: If you use JS files with Tailwind classes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
